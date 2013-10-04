b3aa99addd5b583197b923375458441fc1576b78
Tsao <jtsao@b8f6b1149a0d.ant.amazon.com>
2013-10-03 18:45:40 -0700

    Enabled best bash plugins, aliases, and completions

---
+++ aliases/enabler.sh
@@ -0,0 +1,7 @@
+bash-it enable alias fasd
+bash-it enable alias general
+bash-it enable alias git
+bash-it enable alias homebrew
+bash-it enable alias osx
+bash-it enable alias specific
+bash-it enable alias vim
---
+++ completion/enabler.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+bash-it enable completion bash-it
+bash-it enable completion brew
+bash-it enable completion defaults
+bash-it enable completion git
+bash-it enable completion pip
+bash-it enable completion ssh
+bash-it enable completion tmux
