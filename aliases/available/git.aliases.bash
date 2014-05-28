1477d55c172ee48e4b797796344a7fe5202c44ce
Jason Tsao <jtsao@lab126.com>
2014-05-27 17:47:32 -0700

    Added git completion for git aliases

--- aliases/available/git.aliases.bash
+++ aliases/available/git.aliases.bash
@@ -9,3 +9,14 @@ alias gps='git push $@'
 alias gd='git diff $@'
 alias gl='git log --pretty=oneline --abbrev-commit --color --max-count=50'
 alias gb='git branch'
+
+# Git autocompletion for aliases
+__git_complete grm _git_rm
+__git_complete ga _git_add
+__git_complete gc _git_commit
+__git_complete gco _git_checkout
+__git_complete gpl _git_pull
+__git_complete gps _git_push
+__git_complete gd _git_diff
+__git_complete gl _git_log
+__git_complete gb _git_branch
