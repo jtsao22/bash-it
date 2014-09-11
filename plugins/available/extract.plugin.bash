95c8b9e1ad18f4bd0a8ecd98f5e7644d6445a8e2
Jason Tsao <jtsao@lab126.com>
2014-09-11 15:45:12 -0700

    Added extracting .tar.xz files

--- plugins/available/extract.plugin.bash
+++ plugins/available/extract.plugin.bash
@@ -19,6 +19,7 @@ extract () {
 			*.zip)     unzip $1      ;;
 			*.Z)       uncompress $1 ;;
 			*.7z)      7z x $1       ;;
+			*.xz)      tar xf $1     ;;
 			*)         echo "'$1' cannot be extracted via extract" ;;
 		esac
 	else
