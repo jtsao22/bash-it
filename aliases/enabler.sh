7c18e84207989a6c59b75d5cc63bef9304462973
Tsao <jtsao@u0050b60f5299599df48a.ant.amazon.com>
2017-08-23 18:22:18 -0700

    Remove aliases and and plugins that are no longer used.

--- aliases/enabler.sh
+++ aliases/enabler.sh
@@ -1,7 +1,3 @@
 bash-it enable alias fasd
 bash-it enable alias general
 bash-it enable alias git
-bash-it enable alias homebrew
-bash-it enable alias osx
-bash-it enable alias specific
-bash-it enable alias vim
