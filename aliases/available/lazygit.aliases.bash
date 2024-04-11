8a0708c7fecadf53afd07c2dd302e06d09739b89
Jason Tsao <jtsao@lab126.com>
2024-04-11 20:43:22 +0000

    Add alias for lazygit

---
+++ aliases/available/lazygit.aliases.bash
@@ -0,0 +1,4 @@
+cite about-alias
+about-alias 'lazygit aliases'
+
+alias lz="lazygit"
--- aliases/enabler.sh
+++ aliases/enabler.sh
@@ -2,3 +2,4 @@ bash-it enable alias fasd
 bash-it enable alias general
 bash-it enable alias git
 bash-it enable alias tmux
+bash-it enable alias lazygit
